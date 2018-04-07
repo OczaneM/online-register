f9a67356d3969f759758895ac530e751b54e9d23
Oczane <oczane.rivera@gmail.com>
2018-04-06 21:42:49 -0400

    Setup and install webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './public/index.html',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client')
+        ],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
+          }
+        }
+      }
+    ]
+  }
+};
+
