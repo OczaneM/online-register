c0933c378fc5ab0c84c4b19c4f948b03f11c16b4
Oczane <oczane.rivera@gmail.com>
2018-04-07 10:28:26 -0400

    Add check functions for inputs

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 
-var pages = ['', 'about', 'tournament']
+var pages = ['', 'aboutSection', 'tournamentSection']
 
 $(document).ready(function() {
 	$('#fullpage').fullpage({
@@ -23,20 +23,49 @@ $(document).ready(function() {
 	});
 });
 
-//DOM Element Shortcuts
-var	registerForm = document.getElementById('register-form')
-
-//GET USER INFO
-registerForm.addEventListener('submit', writeUserData(e))
-
 // DATABASE OPERATIONS
 var database = firebase.database()
 
 const writeUserData = (e) => {
 database.ref('tournaments/TXWtHSgxJJa9LUeOMTYy/players').set({
-	firstName: e.target.first,
-	lastName: e.target.last,
-	phone: e.target.phoneNumber,
-	club: e.target.tennisClub
+	firstName: e.target.first.value,
+	lastName: e.target.last.value,
+	phone: e.target.phoneNumber.value,
+	club: e.target.tennisClub.value
 })
 }
+
+
+//DOM Element Shortcuts
+var	registerForm = document.getElementById('register-form')
+var firstName = document.getElementById('first-name')
+var lastName = document.getElementById('last-name')
+var phone = document.getElementById('phone')
+var club = document.getElementById('club')
+
+//Form Functions
+const checkInputs = (e) => {
+	var allChecked = true
+	if (e.target.first.value === '') {
+		e.target.innerHTML = 'Field cannot be empty.'
+		allChecked = false
+	}
+	if (e.target.last.value === '') {
+		e.target.innerHTML = 'Field cannot be empty.'
+		allChecked = false
+	}
+	if (e.target.phoneNumber.value === '') {
+		e.target.innerHTML = 'Field cannot be empty.'
+		allChecked = false
+	}
+	if (allChecked) {
+		writeUserData(e)
+	}
+	else {
+		e.preventDefault()
+	}
+}
+
+//Event Handling
+registerForm.addEventListener('submit', checkInputs)
+
